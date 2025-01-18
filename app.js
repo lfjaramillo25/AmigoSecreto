f299f54dc396334608481d1c2b86199c4a0e982f
Leonardo J <l.jaramillo1019@gmail.com>
2025-01-18 18:30:42 -0500

    validacion del campo para asegurar que no este vacio

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 let amigos = [];
+//funcion que me permite obtener el nombre ingresado por el usuario 
 function agregarAmigo(){
-    amigos.push(document.getElementById("amigo").value);
-    console.log(amigos) 
+    //Se implementa una validación para asegurar de que no se envien campos vacios
+    if(document.getElementById("amigo").value.trim() === ""){
+        //Se envia una alerta al usuario para que verifique el campo que se encuentra vacio
+        alert("Por favor, ingresa un nombre.")
+    }else{
+        //Ingresamos el valor del campo a al array amigo
+        amigos.push(document.getElementById("amigo").value);
+        console.log(amigos)
+    }
+     
 }
