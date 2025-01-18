0f5fdb618600901610284231e905d30339f616b9
Leonardo J <l.jaramillo1019@gmail.com>
2025-01-18 18:46:45 -0500

    Restablecemos el campo a vacio luego de que el usuario ingrese un nombre

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ function agregarAmigo(){
     //Se implementa una validación para asegurar de que no se envien campos vacios
     if(document.getElementById("amigo").value.trim() === ""){
         //Se envia una alerta al usuario para que verifique el campo que se encuentra vacio
-        alert("Por favor, ingresa un nombre.")
+        alert("Por favor, ingresa un nombre.");
     }else{
         //Ingresamos el valor del campo a al array amigo
         amigos.push(document.getElementById("amigo").value);
-        console.log(amigos)
+        console.log(amigos);
+        //Borramos el campo de entrada para que el usuario pueda ingresar nuevamente otro nombre
+        document.getElementById("amigo").value = "";
     }
      
 }
