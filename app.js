3de7398640d5164bc980f01163a006df2db2720f
Leonardo J <l.jaramillo1019@gmail.com>
2025-01-18 19:40:10 -0500

    Se mostro cada amigo ingresado en una lista encima debajo del input

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 let amigos = [];
+let nombre = document.getElementById("amigo");
 //funcion que me permite obtener el nombre ingresado por el usuario 
 function agregarAmigo(){
     //Se implementa una validación para asegurar de que no se envien campos vacios
-    if(document.getElementById("amigo").value.trim() === ""){
+    if(nombre.value.trim() === ""){
         //Se envia una alerta al usuario para que verifique el campo que se encuentra vacio
         alert("Por favor, ingresa un nombre.");
     }else{
         //Ingresamos el valor del campo a al array amigo
-        amigos.push(document.getElementById("amigo").value);
+        amigos.push(nombre.value);
+        //Creamos un nuevo elemento <li>
+        const nombreIngresado = document.createElement('li');
+        nombreIngresado.textContent = nombre.value
+        // Agregar el <li> a la lista <ul>
+        document.getElementById('listaAmigos').appendChild(nombreIngresado)
         console.log(amigos);
         //Borramos el campo de entrada para que el usuario pueda ingresar nuevamente otro nombre
-        document.getElementById("amigo").value = "";
+        nombre.value = "";
+        nombre.focus(); //Enfoca nuevamente el input
     }
      
 }
