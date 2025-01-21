f4abce39e8a95d323c83574a51e202525cd3cef6
Leonardo J <l.jaramillo1019@gmail.com>
2025-01-21 15:03:22 -0500

    Revert "Se mostro cada amigo ingresado en una lista encima debajo del input"
    This reverts commit 3de7398640d5164bc980f01163a006df2db2720f.

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 let amigos = [];
-let nombre = document.getElementById("amigo");
 //funcion que me permite obtener el nombre ingresado por el usuario 
 function agregarAmigo(){
     //Se implementa una validación para asegurar de que no se envien campos vacios
-    if(nombre.value.trim() === ""){
+    if(document.getElementById("amigo").value.trim() === ""){
         //Se envia una alerta al usuario para que verifique el campo que se encuentra vacio
         alert("Por favor, ingresa un nombre.");
     }else{
         //Ingresamos el valor del campo a al array amigo
-        amigos.push(nombre.value);
-        //Creamos un nuevo elemento <li>
-        const nombreIngresado = document.createElement('li');
-        nombreIngresado.textContent = nombre.value
-        // Agregar el <li> a la lista <ul>
-        document.getElementById('listaAmigos').appendChild(nombreIngresado)
+        amigos.push(document.getElementById("amigo").value);
         console.log(amigos);
         //Borramos el campo de entrada para que el usuario pueda ingresar nuevamente otro nombre
-        nombre.value = "";
-        nombre.focus(); //Enfoca nuevamente el input
+        document.getElementById("amigo").value = "";
     }
      
 }
